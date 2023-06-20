6dc8ad9e4eb10c0975358d0377a7beb92215b0a1
emre1n <emre1n@protonmail.com>
2023-06-20 17:17:28 +0300

    add header and footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import styles from './styles.module.css';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer className={styles.footer}>Made with ☕️</footer>;
 };
 
 export default Footer;
